6dc53aa0f170c6b43695853e89ca272ec2561a1e
wrfmartinez <wrmartinez709@gmail.com>
2024-06-20 12:09:57 -0400

    Add https support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   server: {
+    https: true,
     host: '0.0.0.0',
     port: 3001,
   }
